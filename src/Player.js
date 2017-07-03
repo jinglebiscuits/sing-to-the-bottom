f34a5b9df4233588ff1e79a2b7c59db725954638
jinglebiscuits <scott.wehby@gmail.com>
2017-07-03 09:45:17 -0500

    got some communication between player and tween on complete listener

--- src/Player.js
+++ src/Player.js
@@ -2,6 +2,8 @@ Sing.Player = function(game) {
 	this.game = game;
 	this.isShowing = false;
 	this.circle;
+	this._centerY = 0;
+	this._centerX = 0;
 	
 	this.getScreenLocationFromColumn = function(column) {
 		column = Math.min(column, 10);
@@ -16,12 +18,20 @@ Sing.Player = function(game) {
 Sing.Player.prototype = {
 	show: function() {
 		this.circle = game.add.graphics(0, 0);
+		this.circle.owner = this;
 		this.circle.beginFill(0xFF0000, 1);
 		diameter = Sing.COLUMN_SIZE * 0.75;
 		center = Sing.COLUMN_SIZE / 2;
-		this.circle.drawCircle(center, diameter / 2, diameter);
+		this._centerY = diameter / 2 + 30;
+		this._cetnerX = center;
+		this.circle.drawCircle(center, this._centerY, diameter);
 		this.circle.endFill();
 		isShowing = true;
+	},
+
+	startFloating: function() {
+		console.log("float baby");
+		
 	},
 
 	moveTo: function(x = 4, smooth = true) {
@@ -29,10 +39,14 @@ Sing.Player.prototype = {
 			if (smooth) {
 				var slideTween = game.add.tween(this.circle);
 				slideTween.to({
-					centerX: this.getScreenLocationFromColumn(x)
+					centerX: this.getScreenLocationFromColumn(x),
+					centerY: this._centerY
 				}, 450, Phaser.Easing.Cubic.InOut);
-				slideTween.onComplete.add(function() {
+				slideTween.onComplete.add(function(circle, tween) {
 					console.log("cheese balls");
+					console.log("centerX = " + circle.owner.centerX);
+					circle.owner.centerX = circle.centerX;
+					circle.owner.startFloating();
 				});
 				slideTween.start();
 			} else {
@@ -47,6 +61,14 @@ Sing.Player.prototype = {
 	},
 	get pitch() {
 		return this._pitch
+	},
+	set centerX(centerX) {
+		console.log("setting x: " + centerX);
+		this._centerX = centerX;
+	},
+	get centerX() {
+		console.log("returning x: " + this._centerX);
+		return this._centerX;
 	}
 }
 
